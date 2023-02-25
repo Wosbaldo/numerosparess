f18971cad576a2c591d4d6b604dd174a6a0f411a
Wosbaldo <rm0796042022@unab.edu.sv>
2023-02-25 10:52:58 -0600

    Agregar archivos de proyecto.

---
+++ Negocio/ClsNumeros.cs
@@ -0,0 +1,24 @@
+﻿using numerosparess.Entidades;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace numerosparess.Negocio
+{
+    public class ClsNumeros
+    {
+        public void Imprimirnumeros(Numeros numeros)
+        {
+            for(int i = numeros.num1; 1 <= numeros.num2; i++)
+            {
+                if(i % 2 == 0)
+                {
+                    Console.WriteLine(i);
+                }
+
+            }
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿// See https://aka.ms/new-console-template for more information
+//Console.WriteLine("Hello, World!");
+using numerosparess.Entidades;
+using numerosparess.Negocio;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Numeros numeros = new Numeros();
+        ClsNumeros clsnumeros = new ClsNumeros();
+
+
+        Console.WriteLine("Ingresa el numero inical");
+        numeros.num1 = int.Parse(Console.ReadLine());
+
+        Console.WriteLine("Ingresa el numero final");
+        numeros.num2 = int.Parse(Console.ReadLine());
+
+        ClsNumeros.Imprimirnumeros(numeros);
+    }
+}
